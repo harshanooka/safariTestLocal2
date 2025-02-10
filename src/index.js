e90f1541e94b696a1b605da8a81bad238261ab3d
Harsha Nooka <harshavardhannooka@gmail.com>
2025-02-09 18:06:15 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 const iframe = document.createElement("iframe");
-iframe.src = "https://local.cache.com:8081";
+iframe.src = "https://safari-test-one.vercel.app";
 iframe.style.display = "none";
 document.body.appendChild(iframe);
-
-function checkCookie(name) {
-  return document.cookie.split("; ").some((row) => row.startsWith(name + "="));
-}
-
-function ensureStorageAccess() {
-  if (!checkCookie("storageUnlocked")) {
-    console.log("🚨 Redirecting to grant storage access...");
-    window.location.href = "https://local.cache.com:8081/grant-access.html";
-  } else {
-    console.log("✅ Storage access already granted!");
-  }
-}
 
 function sendMessage(action, key, value = null) {
   return new Promise((resolve) => {
     function listener(event) {
-      if (event.origin === "https://local.cache.com:8081") {
+      if (event.origin === "https://safari-test-one.vercel.app") {
         resolve(event.data.value);
         window.removeEventListener("message", listener);
       }
     }
     window.addEventListener("message", listener);
-    iframe.contentWindow?.postMessage({ action, key, value }, { targetOrigin: "https://local.cache.com:8081" });
+    iframe.contentWindow?.postMessage({ action, key, value }, "https://safari-test-one.vercel.app");
   });
 }
 
@@ -39,9 +26,19 @@ function setLightTheme() {
 function getTheme() {
   sendMessage("get", "theme").then((value) => {
     document.body.style.background = value;
-
     console.log("Theme from IndexedDB:", value);
   });
 }
 
-ensureStorageAccess();
+async function triggerRequestStorageAccess() {
+  if (document.requestStorageAccess) {
+    try {
+      await document.requestStorageAccess();
+      console.log("Storage access granted!");
+    } catch (error) {
+      console.warn("Storage access denied:", error);
+    }
+  } else {
+    console.log("Storage Access API not needed in this browser.");
+  }
+}
